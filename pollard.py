79802ad1e07d329c746b102dbeb99f6ecfb01926
IrrinaShim <101702637+IrrinaShim@users.noreply.github.com>
2022-12-29 14:47:48 +0300

    Create pollard.py

---
+++ pollard.py
@@ -0,0 +1,52 @@
+import random
+import  math
+
+def factorial(a):
+    s = 1
+    for i in range(2, a+1):
+        s = s*i;
+    return s
+
+def NOD(a, b):
+    if (a == 0 or b == 0):
+        return (a+b)
+    elif (a>b):
+        return(a - b, b)
+    else:
+        return(a, b - a)
+    
+def get_bk(a, k, n):
+    # вернёт (a ** w) % n
+#    k = factorial(k)
+    k = bin(k)[2:][::-1]
+    s = 1
+    c = a
+    for i in range(len(k)):
+        if k[i] == '1':
+            s = (s * c) % n
+        c = (c * c) % n
+    s = s - 1
+    if (s < 0):
+        s = s + n
+    return s
+    
+n = int(input())
+N = int(input())
+
+flag = False;
+a = 2;
+
+while((a < n) and not flag):
+    for k in range(1, N):
+        b_k = get_bk(a, k, n)
+        p = NOD(b_k, n);
+        if (p != 1 and p != n):
+            p = b_k
+            flag = True
+        if flag:
+            break
+
+if flag:
+    print(p)
+else:
+    print(1)
